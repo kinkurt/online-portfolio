329a0dc71c4a88e087e924d8e474987be4cd2fd1
Kurt <kin.kurt@gmail.com>
2021-08-19 17:54:09 +0200

    fixed pages and folders changed intro text and nav layout

---
+++ components/Intro/index.js
@@ -0,0 +1,27 @@
+
+import { Wrapper, Content, ProfileImg } from './Intro.styles'
+import { Element } from 'react-scroll';
+
+const Intro = () => (
+    <Element name="intro" className="element" >
+    <Wrapper>
+        <Content>
+             <ProfileImg src= {"/KKpic.jpg"} alt='Profile pic' />
+                <div>
+                <h1>
+                    I&apos;m 
+                     <span> Kurt</span> Kin</h1>
+                
+                <p>
+                    This is my online portfolio where I try to implement all the knowledge I have learnt.
+                </p>
+                </div>
+                
+
+        </Content>
+    </Wrapper>
+    </Element>
+);
+
+
+export default Intro;
