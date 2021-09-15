0e17e78cf60123845c01c63605994edc54a88e2b
Kurt <kin.kurt@gmail.com>
2021-09-15 03:20:52 +0200

    retrying country image

--- data/projects.data.js
+++ data/projects.data.js
@@ -2,7 +2,7 @@ export const projects = [
 {
     title:"Countries info",
     description:"Api and material-ui site",
-    image: '/CountriesApi.png',
+    image: '/countriesinfo.png',
     link: "https://kind-spence-22f7b7.netlify.app/",
 }
 ];
